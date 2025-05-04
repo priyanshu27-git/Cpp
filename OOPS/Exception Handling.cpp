2f156b3ca2b8a5f9ece6f42481f2df3060f8ab8b
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-05-04 23:06:21 +0530

    meaning of exception handling

---
+++ OOPS/Exception Handling.cpp
@@ -0,0 +1,10 @@
+//                              [RUNTIME ERROR IS C/D EXCEPTION]
+//exception are mostly run time error that occur during the execution of the code
+/* Ecxception handling
+A systematic and graceful way of handling run time errors so that these errors are visible to the end users so that they can understand the raeson behind it
+To handle exception in c++ there are 3 keywords taht are need to remember-1. Try 2. Catch 3.Throw
+Throw - Throw keyword is used to bypass the lines  that occur just after the line that is prone to error 
+
+
+
+*/
